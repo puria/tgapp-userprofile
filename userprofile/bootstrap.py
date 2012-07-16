78427742ff5473e659eb1ba7955b79fdeb9a0fbb
Alessandro Molina <alessandro.molina@axant.it>
2012-07-16 11:39:54 +0200

    stray model got removed

--- userprofile/bootstrap.py
+++ userprofile/bootstrap.py
@@ -6,10 +6,3 @@ from tgext.pluggable import app_model
 
 def bootstrap(command, conf, vars):
     print 'Bootstrapping userprofile...'
-
-    s1 = model.Sample()
-    s1.name = 'Test Sample'
-    s1.user = model.DBSession.query(app_model.User).first()
-
-    model.DBSession.add(s1)
-    model.DBSession.flush()
