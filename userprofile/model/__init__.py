9afaea0d7dcc5bad11727e1c91cbddec4101a7bc
Alessandro Molina <alessandro.molina@axant.it>
2016-09-02 19:09:57 +0200

    Fixes for ming

--- userprofile/model/__init__.py
+++ userprofile/model/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy.ext.declarative import declarative_base
 from tgext.pluggable import PluggableSession
 
 DBSession = PluggableSession()
-DeclarativeBase = declarative_base()
 
 
 def init_model(app_session):
