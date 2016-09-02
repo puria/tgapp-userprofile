ebf368d9df174e77541eefc63faf2454e872e19b
Alessandro Molina <alessandro.molina@axant.it>
2016-09-02 19:10:30 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except IOError:
 
 setup(
     name='tgapp-userprofile',
-    version='0.1.0',
+    version='0.1.1',
     description='''Pluggable application for TurboGears2 which provides a basic user profile page with
 forms to allow users to edit their own profile or change their password''',
     long_description=README,
