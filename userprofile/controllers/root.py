25f7faeb0370fe0a6ac5a1681c27d60ec0e01b10
Marco Bosio <marco.bosio@axant.it>
2018-02-05 17:58:35 +0100

    Check for password field: now is not mandatory

--- userprofile/controllers/root.py
+++ userprofile/controllers/root.py
@@ -66,11 +66,13 @@ class RootController(TGController):
             profile_save = update_user_data
 
         # do not change the password if the user did't set it
-        if not kw.get('password'):  # keep old email
-            kw.pop('password')
-            kw.pop('verify_password')
-        else:  # set new email
-            kw.pop('verify_password')
+
+        if 'password' in kw:
+            if not kw.get('password'):  # keep old email
+                kw.pop('password')
+                kw.pop('verify_password')
+            else:  # set new email
+                kw.pop('verify_password')
 
         # we don't want to save the email until it is confirmed
         new_email = kw['email_address']
